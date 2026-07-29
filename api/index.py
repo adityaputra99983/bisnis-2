10dc4cf4f3771bb4285d5da9be4f43d36a61392f
adityaputra99983 <adityaputra@gmail.com>
2026-07-29 11:58:00 +0700

    fix: use Supabase IPv4 pooler host + sslmode

--- api/index.py
+++ api/index.py
@@ -6,7 +6,7 @@ if _root not in sys.path:
     sys.path.insert(0, _root)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dukun.settings')
-os.environ.setdefault('DATABASE_URL', 'postgresql://postgres.vwjxhfpvcaesrfcglgex:%2B%3FV%2FQxPVcU%24y93c@db.vwjxhfpvcaesrfcglgex.supabase.co:5432/postgres')
+os.environ.setdefault('DATABASE_URL', 'postgresql://postgres.vwjxhfpvcaesrfcglgex:%2B%3FV%2FQxPVcU%24y93c@aws-1-ap-northeast-2.pooler.supabase.com:6543/postgres?pgbouncer=true&sslmode=require')
 
 from django.core.wsgi import get_wsgi_application
 app = get_wsgi_application()
