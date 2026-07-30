3673ffd3c44f87217ed20d4151b88f3e74e58c17
adityaputra99983 <adityaputra@gmail.com>
2026-07-30 11:36:28 +0700

    Merge

--- api/index.py
+++ api/index.py
@@ -9,5 +9,14 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dukun.settings')
 os.environ.setdefault('DATABASE_URL', 'postgresql://postgres.vwjxhfpvcaesrfcglgex:%2B%3FV%2FQxPVcU%24y93c@aws-1-ap-northeast-2.pooler.supabase.com:6543/postgres?sslmode=require')
 os.environ.setdefault('SUPABASE_SERVICE_KEY', '')
 
+import django
+django.setup()
+
+from django.core.management import call_command
+try:
+    call_command('migrate', '--noinput', verbosity=0)
+except Exception:
+    pass
+
 from django.core.wsgi import get_wsgi_application
 app = get_wsgi_application()
