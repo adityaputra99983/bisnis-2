872499224196c9e58aada7067bfa4498a1dbc905
adityaputra99983 <adityaputra@gmail.com>
2026-08-01 10:49:34 +0700

    update fitur

--- dukun/settings.py
+++ dukun/settings.py
@@ -156,7 +156,10 @@ CACHES = {
 }
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
